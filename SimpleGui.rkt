adde95d9739fb277797a9e000e6aa0143b489bc7
yran1980war <yran8002@yandex.ru>
2018-03-10 17:51:55 +0300

    was added increment and decrement of value

--- SimpleGui.rkt
+++ SimpleGui.rkt
@@ -1,20 +1,34 @@
 #lang racket
  (require racket/gui/base)
+
 (define frame
-(new frame%
-  [label "Example"]
-(width 500)
-(height 500)))
+  (new frame%
+       [label "Example"]
+       (width 500)
+       (height 500)))
+
 (define msg
- (new message%
-   [parent frame]
-[label "первое окно"]))
-(new button% [parent frame]
-             [label "нажать"]
-[callback (lambda (button event)
-             (send msg set-label "ок"))])
-(new button% [parent frame]
-             [label "нажать"]
-             [callback (lambda (button event) (sleep 5))])
+  (new message%
+       [parent frame]
+       (stretchable-width #t)
+       [label "первое окно"]))
+
+(define cnt 0)
+
+(new button%
+     [parent frame]
+     [label "нажать"]
+     [callback
+      (lambda (button event)
+        (set! cnt (+ 1 cnt))
+        (send msg set-label (format "значение cnt = ~a" cnt)))])
+
+(new button%
+     [parent frame]
+     [label "нажать"]
+     [callback
+      (lambda (button event)
+        (set! cnt (- cnt 1 ))
+        (send msg set-label (format "значение cnt = ~a" cnt)))])
 
 (send frame show #t)
